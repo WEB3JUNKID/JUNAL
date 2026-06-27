d2dfc8ed710bd4d3441990bdf9beaa6ceacf50a2
WEB3JUNKID <victoruchenna2007@gmail.com>
2026-06-27 22:21:09 +0100

    Update service worker for caching and logging

--- sw.js
+++ sw.js
@@ -1,25 +1,46 @@
-const CACHE_NAME = 'euphoria-journal-v3';
+
+const CACHE_NAME = 'liquidity-log-v1';
 const ASSETS_TO_CACHE = [
-    './',
-    './index.html',
-    './manifest.json',
+  './',
+  './index.html',
+  './manifest.json',
     './icon.png',
-    './firebase.js',
-    './chart.js'
+  // You can add your icon filenames here once created:
+  // './icon-192.png',
+  // './icon-512.png'
 ];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(ASSETS_TO_CACHE);
-        })
-    );
+// Install Event: Cache our core files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+// Activate Event: Clean up old caches if we update the app
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
         })
-    );
+      );
+    })
+  );
+});
+
+// Fetch Event: Serve from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return the cached version if found, otherwise fetch from network
+      return response || fetch(event.request);
+    })
+  );
 });
